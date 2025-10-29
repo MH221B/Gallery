07711f8e964f100416bdaf882561925c47ebe2d5
MH221B <22127131@student.hcmus.edu.vn>
2025-10-29 21:40:26 +0700

    fix: update HeaderComponent to use Link for navigation

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,13 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 const HeaderComponent = () => {
   return (
-    <Navbar expand="lg" bg="dark" variant="dark">
-        <Container>
-            <Navbar.Brand href="/">Gallery</Navbar.Brand>
-        </Container>
-    </Navbar>
+  <Navbar expand="lg" bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand as={Link} to="/">Gallery</Navbar.Brand>
+    </Container>
+  </Navbar>
   )
 }
 
